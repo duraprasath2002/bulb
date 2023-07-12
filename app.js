265b3bcfb2b23c2e8931b3026107758e4843b178
duraprasath2002 <139108340+duraprasath2002@users.noreply.github.com>
2023-07-12 11:13:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+     
+     img.src="./bulb on.png";
+     
+
+
+}
+function bulb_off(){
+    img.src="./bulb off.jfif";
+}
